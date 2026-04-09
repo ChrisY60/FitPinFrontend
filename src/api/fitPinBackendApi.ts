c9c0ab5074e3f78cfaff040c4b2aa5bf9a65ee61
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2026-04-10 00:09:17 +0200

    Initial commit - TypeScript React frontend

---
+++ src/api/fitPinBackendApi.ts
@@ -0,0 +1,12 @@
+import apiClient from "./apiClient";
+
+export type RegisterRequest = {
+	username: string;
+	emailAddress: string;
+	password: string;
+}
+
+export async function registerUser(data: RegisterRequest) {
+	const response = await apiClient.post("/users/register", data);
+	return response.data;
+}
---
+++ src/components/RegistrationForm.tsx
@@ -0,0 +1,70 @@
+
+import type { RegisterRequest } from "../api/fitPinBackendApi";
+import { useState } from "react";
+import { registerUser } from "../api/fitPinBackendApi";
+function RegistrationForm (){
+	const [formData, setFormData] = useState<RegisterRequest>({
+		username: "",
+		emailAddress: "",
+		password: "",
+	});
+
+    const [message, setMessage] = useState<string>("");
+
+	const handleSubmit = async () => {
+		try {
+			setMessage("Registration successful");
+			const response = await registerUser(formData);
+			console.log("Registration successful:", response);
+		} catch (error) {
+			setMessage(error.response?.data?.message ?? "Something went wrong");
+			console.log("Registration failed:", error);
+		}
+	}
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+
+	return ( 
+		<div>
+
+			<h2>Register</h2>
+
+			{message && <p>{message}</p>}
+			<input
+				name="username"
+				type="text"
+				placeholder="Username"
+				value={formData.username}
+				onChange={handleChange}
+			/>
+
+
+			<input 
+				name="emailAddress"
+				type="email"
+				placeholder="Email"
+				value={formData.emailAddress}
+				onChange={handleChange}
+			/>
+
+			<input
+				name="password"
+				type="password"
+				placeholder="password"
+				value={formData.password}
+				onChange={handleChange}
+			/>
+
+			<button onClick={handleSubmit}>Register</button>
+
+		</div>
+	)
+}
+
+export default RegistrationForm;
